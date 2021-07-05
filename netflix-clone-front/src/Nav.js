fe03c3701ea3ef2bf681899c082f8d9c231c8f40
Anthony Herrera <ankehecr100@gmail.com>
2021-07-05 12:50:11 -0500

    added navbar and trailer popups

--- netflix-clone-front/src/Nav.js
+++ netflix-clone-front/src/Nav.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import './Nav.css';
 
 function Nav() {
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if(window.scrollY > 1) {
+                handleShow(true);
+            }else {
+                handleShow(false);
+            }
+        });
+        return () => {
+            window.removeEventListener('scroll');
+        }
+    }, []);
+
     return (
-        <div>
-            
+        <div className={`nav ${show && "nav__black"}`}>
+            <img
+                className="nav__logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+                alt="Netflix Logo"
+            />
+            <img
+                className="nav__avatar"
+                src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+                alt="Netflix Logo"
+            />
         </div>
-    )
+    );
 }
 
-export default Nav
+export default Nav;
--- netflix-clone-front/src/Row.js
+++ netflix-clone-front/src/Row.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from 'movie-trailer';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
 
     useEffect(() => {
         async function fetchData() {
@@ -17,6 +20,27 @@ function Row({ title, fetchUrl, isLargeRow }) {
     }, [fetchUrl]);
 
     //console.table(movies);
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters
+            autoplay: 1,
+        },
+    };
+
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl("");
+        } else {
+            movieTrailer(movie?.name || movie?.title || movie?.original_title || "")
+                .then((url) => {
+                    const urlParams = new URLSearchParams(new URL(url).search); 
+                    setTrailerUrl(urlParams.get("v"));
+                })
+                .catch((error) => console.log(error));
+        }
+    };
 
     return (
         <div className="row">
@@ -25,12 +49,18 @@ function Row({ title, fetchUrl, isLargeRow }) {
                 {movies.map((movie) => (
                     <img
                         key={movie.id}
-                        className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                        onClick={handleClick(movie)}
+                        className={`row__poster ${
+                            isLargeRow && "row__posterLarge"
+                        }`}
+                        src={`${base_url}${
+                            isLargeRow ? movie.poster_path : movie.backdrop_path
+                        }`}
                         alt={movie.name}
                     />
                 ))}
             </div>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     );
 }
